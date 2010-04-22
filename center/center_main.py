f4021d2584bbde3cdffd2d3ca02c6907f596edbe
yanf.guo <yanf.guo@636d5687-d6de-0525-1b07-8761296216f2>
2010-04-22 02:26:13 +0000

    1. Starting building main structure of center and test

--- center/center_main.py
+++ center/center_main.py
@@ -9,6 +9,7 @@ __date__ ="$Apr 8, 2010 3:55:56 PM$"
 import os, sys, time
 import signal
 
+import daemonize
 import center_global, comm_service
 
 ISOTIMEFMT='%Y-%m-%d %X'
@@ -19,8 +20,37 @@ def sigint_handler(signum, frame):
     stopAll()
     print 'catched interrupt signal!'
 
-if __name__ == "__main__":
-#    print "Hello World";
+def main(argv):
     signal.signal(signal.SIGINT, sigint_handler)
     center_global.global_init()
     comm_service.start_center()
+    center_global.cleanup_exit()
+
+if __name__ == "__main__":
+    center_global.set_global('is_daemonized', False)
+    center_global.set_global('is_monitor', False)
+    center_global.set_global('is_daemon_log', False)
+    shortopts = 'dlmsp:'
+#    longopts = ['hadoop-test-mode', 'start-agent', 'stop-agent', 'test']
+    test = '-a -d -m test -m -p'
+    try:
+        opts, argv = getopt.getopt(sys.argv[1:], shortopts)
+    except getopt.GetoptError, err:
+        print str(err)
+        sys.exit(2)
+    print opts
+    print argv
+    for o, a in opts:
+        if o == '-d':
+            center_global.set_global('is_daemonized', True)
+        elif o == '-m':
+            center_global.set_global('is_monitor', True)
+        elif o == '-l':
+            center_global.set_global('is_daemon_log', True)
+
+    print center_global.get_global('is_daemonized')
+    print center_global.get_global('is_monitor')
+    print center_global.get_global('is_daemon_log')
+    if center_global.get_global('is_daemonized') == True:
+        daemonize.daemonize()
+    main(argv)
