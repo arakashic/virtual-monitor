71da8d31373f23095924af0456bc2623ca48fdb6
yanf.guo <yanf.guo@636d5687-d6de-0525-1b07-8761296216f2>
2010-05-15 02:44:21 +0000

    1. apply changes in site pages and css 2. add views module 3. add template loading path and static media path in setting module

--- center_web/settings.py
+++ center_web/settings.py
@@ -1,4 +1,5 @@
 # Django settings for center_web project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -35,12 +36,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = './site/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/site/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -69,6 +70,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'site').replace('\\', '/')
 )
 
 INSTALLED_APPS = (
---
+++ center_web/views.py
@@ -0,0 +1,12 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Yanfei Guo"
+__date__ ="$May 15, 2010 10:25:43 AM$"
+
+
+
+ISOTIMEFMT='%Y-%m-%d %X'
+
+if __name__ == "__main__":
+    print "Hello World";
